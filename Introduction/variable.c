e38d721e88139bafdee3201f3e1c456150abdad0
djesposi-5957 <djesposi@usc.edu>
2026-07-14 14:14:57 -0700

    My very first variable

---
+++ Introduction/variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Always declare variables before using them in C.
+    int some_variable = 50;
+    printf("The value of some_variable is: %d\n", some_variable);
+    // The %d format specifier is used to print an integer value.
+    // The value of some_variable is printed to the console.
+
+    return 0;
+}
